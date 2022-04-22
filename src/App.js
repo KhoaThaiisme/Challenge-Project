100104bb93c59dd2b61c77c4b1fa3b3387f23b31
KhoaThaiisme <khoathai@khoas-MBP.lan>
2022-04-22 14:24:12 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
@@ -9,6 +9,8 @@ function App() {
   Define state variables for 
   contacts and appointments 
   */
+  const [appointments, setAppointments] = useState([]);
+  const [contacts, setContacts] = useState([]);
 
   const ROUTES = {
     CONTACTS: "/contacts",
@@ -19,6 +21,27 @@ function App() {
   Implement functions to add data to
   contacts and appointments
   */
+  const addAppointment = (title, contact, date, time) => {
+    setAppointments([
+      ...appointments,
+      {
+        title: title,
+        contact: contact,
+        date: date,
+        time: time,
+      },
+    ])
+  };
+
+  const addContact = (name, phone, email) => {
+    setContacts([
+      ...contacts, {
+        name: name,
+        phone: phone,
+        email: email,
+      },
+    ])
+  }
 
   return (
     <>
@@ -37,11 +60,11 @@ function App() {
           </Route>
           <Route path={ROUTES.CONTACTS}>
              {/* Add props to ContactsPage */}
-            <ContactsPage />
+            <ContactsPage contacts={contacts} addContact={addContact}/>
           </Route>
           <Route path={ROUTES.APPOINTMENTS}>
             {/* Add props to AppointmentsPage */}
-            <AppointmentsPage />
+            <AppointmentsPage appointments={appointments} addAppointment={addAppointment} contacts={contacts}/>
           </Route>
         </Switch>
       </main>
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-export const ContactsPage = () => {
+import { ContactForm } from '../../components/contactForm/ContactForm';
+import { TileList } from '../../components/tileList/TileList';
+export const ContactsPage = ({contacts, addContact}) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [duplicate, setDuplicate] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,7 +18,28 @@ export const ContactsPage = () => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+   if(!duplicate) {
+     addContact(name, phone, email);
+     setName('');
+     setPhone('');
+     setEmail('');
+   }
   };
+
+    useEffect(() => {
+      const nameIsDuplicate = () => {
+        const found = contacts.find((contact) => contact.name === name);
+        if(found !== undefined){
+          return true;
+        }
+        return false;
+      }
+      if(nameIsDuplicate()){
+        setDuplicate(true);
+      } else {
+        setDuplicate(false);
+      }
+    }, [name, contacts, duplicate])
 
   /*
   Using hooks, check for contact name in the 
@@ -22,11 +49,24 @@ export const ContactsPage = () => {
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>
+          Add Contact
+          {duplicate ? " - Name Already Exists" : ""}
+        </h2> 
+        <ContactForm 
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          email={email}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList tiles={contacts} />
       </section>
     </div>
   );
